82e0d3fa3ecae20b710778c1e10d649a5a00736e
pantasio <hoang.lduc@gmail.com>
2015-03-19 23:09:56 +0700

    cau hinh easyengine

--- runthis.sh
+++ runthis.sh
@@ -1,1 +1,6 @@
-Install yaourt, git, gitg,  
+Install yaourt, git, gitg, sublime-text, virtualbox, vagrant, guake, youtube-dl
+
+
+Add auto start some program
+	Name			Command			Description
+	Guake 			guake 			Run guake terminal
